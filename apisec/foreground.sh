9d19f8430e4ad6562cb28f761c1842d107eaeece
Imran Shaik <33627789+imranshaik012@users.noreply.github.com>
2020-03-09 22:01:13 +0530

    scenarious14

--- apisec/foreground.sh
+++ apisec/foreground.sh
@@ -1,8 +1,8 @@
 
-show_progress1()
-{
-sleep 1;echo "Hello..."  
-sleep 1; wait.sh
+# show_progress1()
+# {
+# sleep 1;echo "Hello..."  
+# sleep 1; wait.sh
 
-}
-show_progress1
+# }
+# show_progress1
