3c95b42c1903d7aadb2c61e7df0ad59f009b1d48
Imran Shaik <33627789+imranshaik012@users.noreply.github.com>
2020-03-09 22:13:46 +0530

    scenarious22

--- apisec/background.sh
+++ apisec/background.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-show_progress2()
-{
 echo "Hello..." >> /root/katacoda-finished
 sleep 5
 echo "done" >> /root/katacoda-finished
 sleep 5
 echo "done" >> /root/katacoda-background-finished
-}
-
-show_progress2
