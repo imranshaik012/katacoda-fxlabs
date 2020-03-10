2e252d1f3d2f76ba5d842fae0971bc5da2d14e1f
Imran Shaik <33627789+imranshaik012@users.noreply.github.com>
2020-03-10 12:56:03 +0530

    scenarious70

--- apisec/background.sh
+++ apisec/background.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+echo "Hello..." >> /root/katacoda-finished
+sleep 2
+echo "done" >> /root/katacoda-finished
+sleep 
+echo "done" >> /root/katacoda-background-finished
--- apisec/foreground.sh
+++ apisec/foreground.sh
@@ -1,1 +1,2 @@
-#!/bin/bash
+sleep 1; echo "Hello..."  
+sleep 1; wait.sh  >> /dev/null
